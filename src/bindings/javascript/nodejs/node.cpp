f4a40b8c0d699498347b58ead2d7d71c9d2b56c2
stepbackio <54059380+stepbackio@users.noreply.github.com>
2019-08-13 17:35:40 +0200

    Create node.cpp

---
+++ src/bindings/javascript/nodejs/node.cpp
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <string>
+#include <napi.h>
+
+#include "../lib/fibonacci.cpp"
+
+using namespace Napi;
+
+Value Fibonacci(const CallbackInfo& info) {
+  assert(info[0].IsNumber());
+  unsigned int n = info[0].As<Number>().Uint32Value();
+  auto result = fibonacci(n);
+  return Number::New(info.Env(), result);
+}
+
+Object Init(Env env, Object exports) {
+  exports.Set("fibonacci", Function::New(env, Fibonacci));
+  return exports;
+}
+NODE_API_MODULE(addon, Init)
